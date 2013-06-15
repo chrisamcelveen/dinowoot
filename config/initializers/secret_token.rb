9508f5a1c5eb6258db54bab55fc57a862c8004a7
Christopher McElveen <camcelve@Christophers-MacBook-Air.local>
2013-06-15 18:44:12 -0400

    My first rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinowoot::Application.config.secret_token = 'b693c66516374f6e1048602f1ca6e57f0b0e72904a5eb5980b4edc10f87ab3c70d4ae540ae38dfeb8484502605563de6a66621faa9c7bfd7d0c24b175a9573d8'
